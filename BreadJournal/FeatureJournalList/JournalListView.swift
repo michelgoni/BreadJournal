7108a01d254ce6af999e3582903edefc6daeed08
Michel <michelgoni2@gmail.com>
2024-01-09 19:58:13 +0100

    Improved empty state

--- BreadJournal/FeatureJournalList/JournalListView.swift
+++ BreadJournal/FeatureJournalList/JournalListView.swift
@@ -62,50 +62,50 @@ struct BreadJournalListView: View {
         WithViewStore(self.store,
                       observe: \.journalEntries) { viewStore in
             NavigationStack {
-                if viewStore.state.isEmpty {
-                    EmptyView()
-                } else {
-                    ScrollView {
-                        LazyVGrid(
-                            columns: columns,
-                            spacing: 16) {
+                ScrollView {
+                    LazyVGrid(
+                        columns: columns,
+                        spacing: 16) {
+                            if viewStore.isEmpty {
+                                EmptyView()
+                            } else {
                                 ForEach(viewStore.state) { entry in
                                     JournalEntryView.init(entry: entry)
                                 }
                             }
-                            .padding(.all, 46)
-                    }
-                    .navigationTitle("Bread journal")
-                    .toolbar {
-                        ToolbarItemGroup(placement: .primaryAction) {
-                            Button {
-                                viewStore.send(.addEntry)
-                          
-                            } label: {
-                                Image(systemName: "plus.circle.fill")
-                                    .foregroundColor(.black)
-                                    .font(
-                                        .system(
-                                            size: 40,
-                                            weight: .light)
-                                    )
-                            }
-                            .padding(.top, 48)
-                            Spacer()
-                            Button {
-                                viewStore.send(.filterEntries)
-                               
-                            } label: {
-                                Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                                    .foregroundColor(.black)
-                                    .font(
-                                        .system(
-                                            size: 40,
-                                            weight: .light)
-                                    )
-                            }
-                            .padding(.top, 48)
                         }
+                        .padding(.all, 46)
+                }
+                .navigationTitle("Bread journal")
+                .toolbar {
+                    ToolbarItemGroup(placement: .primaryAction) {
+                        Button {
+                            viewStore.send(.addEntry)
+                      
+                        } label: {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.black)
+                                .font(
+                                    .system(
+                                        size: 40,
+                                        weight: .light)
+                                )
+                        }
+                        .padding(.top, 48)
+                        Spacer()
+                        Button {
+                            viewStore.send(.filterEntries)
+                           
+                        } label: {
+                            Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                                .foregroundColor(.black)
+                                .font(
+                                    .system(
+                                        size: 40,
+                                        weight: .light)
+                                )
+                        }
+                        .padding(.top, 48)
                     }
                 }
                 

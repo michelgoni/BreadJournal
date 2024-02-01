4986e280badebaa5e7570bc7831e449b308ad9f9
Michel <michelgoni2@gmail.com>
2024-02-01 15:05:11 +0100

    Removed NavigationStack

--- BreadJournal/FeatureJournalDetail/JournalDetailView.swift
+++ BreadJournal/FeatureJournalDetail/JournalDetailView.swift
@@ -32,103 +32,100 @@ struct JournalDetailView: View {
     @Bindable var store: StoreOf<JournalDetailViewFeature>
     
     var body: some View {
-       
-            NavigationStack {
-                VStack {
-                    Form {
-                        Section(header: Text("Fecha")) {
-                            Text(store.journalEntry.entryDate.convertToMonthYearFormat())
-                        }
-                        Section(header: Text("Ingredientes")) {
-                            ForEach(store.ingredients) {
-                                Text($0.ingredient)
-                            }
-                            
-                        }
-                        
-                        Section(header: Text("Foto")) {
-                            Image(uiImage: store.journalEntry.breadPicture ?? UIImage())
-                        }
-                        
-                        Group {
-                            Section(header: Text("Hora último refresco mada madre")) {
-                                Text(store.journalEntry.lastSourdoughFeedTime.toHourMinuteString())
-                            }
-                            Section(header: Text("Hora comiezo prefermento")) {
-                                Text(store.journalEntry.prefermentStartingTime.toHourMinuteString())
-                            }
-                            Section(header: Text("Hora comiezo autólisis")) {
-                                Text(store.journalEntry.autolysisStartingTime.toHourMinuteString())
-                            }
-                            Section(header: Text("Hora comiezo fermentación en bloque")) {
-                                Text(store.journalEntry.bulkFermentationStartingTime.toHourMinuteString())
-                            }
-                            
-                            Section(header: Text("Pliegues")) {
-                                Text(store.journalEntry.folds)
-                            }
-                            Section(header: Text("Hora formado del pan")) {
-                                Text(store.journalEntry.breadFormingTime.toHourMinuteString())
-                            }
-                            Section(header: Text("Hora segunda fermentación")) {
-                                Text(store.journalEntry.secondFermentarionStartingTime.toHourMinuteString())
-                            }
-                            Group {
-                                Section(header: Text("¿Se ha usado frigorífico?")) {
-                                    Text(store.journalEntry.isFridgeUsed.elementUsedTitle)
-                                }
-                                
-                                if store.journalEntry.isFridgeUsed {
-                                    Section(header: Text("Tiempo total en el frigo")) {
-                                        Text(store.journalEntry.fridgeTotalTime)
-                                    }
-                                }
-                                Section(header: Text("Tiempo de horneado")) {
-                                    Text(store.journalEntry.bakingTime)
-                                }
-                                Section(header: Text("¿Plancha de acero?")) {
-                                    Text(store.journalEntry.isSteelPlateUsed.elementUsedTitle)
-                                }
-                            }
-                            Section(header: Text("Corteza")) {
-                                StarRatingView(staticRating: store.journalEntry.crustRating)
-                            }
-                            Section(header: Text("Miga")) {
-                                StarRatingView(staticRating: store.journalEntry.crumbRating)
-                            }
-                            Section(header: Text("Subida")) {
-                                StarRatingView(staticRating: store.journalEntry.bloomRating)
-                            }
-                            Section(header: Text("Greñado")) {
-                                StarRatingView(staticRating: store.journalEntry.scoreRating)
-                            }
-                            Section(header: Text("Sabor")) {
-                                StarRatingView(staticRating: store.journalEntry.tasteRating)
-                            }
-                            Section(header: Text("Evaluation")) {
-                                StarRatingView(staticRating: store.journalEntry.evaluation)
-                            }
-                        }
-                        
-                        Section {
-                            Button("Delete") {
-                                
-                            }
-                            .foregroundColor(.red)
-                            .frame(maxWidth: .infinity)
-                        }
-                        
+        
+        VStack {
+            Form {
+                Section(header: Text("Fecha")) {
+                    Text(store.journalEntry.entryDate.convertToMonthYearFormat())
+                }
+                Section(header: Text("Ingredientes")) {
+                    ForEach(store.ingredients) {
+                        Text($0.ingredient)
                     }
-                    .toolbar {
-                        Button("Edit") {
-                            
-                        }
-                    }
-                    .navigationTitle(store.journalEntry.name)
+                    
                 }
                 
-            
+                Section(header: Text("Foto")) {
+                    Image(uiImage: store.journalEntry.breadPicture ?? UIImage())
+                }
+                
+                Group {
+                    Section(header: Text("Hora último refresco mada madre")) {
+                        Text(store.journalEntry.lastSourdoughFeedTime.toHourMinuteString())
+                    }
+                    Section(header: Text("Hora comiezo prefermento")) {
+                        Text(store.journalEntry.prefermentStartingTime.toHourMinuteString())
+                    }
+                    Section(header: Text("Hora comiezo autólisis")) {
+                        Text(store.journalEntry.autolysisStartingTime.toHourMinuteString())
+                    }
+                    Section(header: Text("Hora comiezo fermentación en bloque")) {
+                        Text(store.journalEntry.bulkFermentationStartingTime.toHourMinuteString())
+                    }
+                    
+                    Section(header: Text("Pliegues")) {
+                        Text(store.journalEntry.folds)
+                    }
+                    Section(header: Text("Hora formado del pan")) {
+                        Text(store.journalEntry.breadFormingTime.toHourMinuteString())
+                    }
+                    Section(header: Text("Hora segunda fermentación")) {
+                        Text(store.journalEntry.secondFermentarionStartingTime.toHourMinuteString())
+                    }
+                    Group {
+                        Section(header: Text("¿Se ha usado frigorífico?")) {
+                            Text(store.journalEntry.isFridgeUsed.elementUsedTitle)
+                        }
+                        
+                        if store.journalEntry.isFridgeUsed {
+                            Section(header: Text("Tiempo total en el frigo")) {
+                                Text(store.journalEntry.fridgeTotalTime)
+                            }
+                        }
+                        Section(header: Text("Tiempo de horneado")) {
+                            Text(store.journalEntry.bakingTime)
+                        }
+                        Section(header: Text("¿Plancha de acero?")) {
+                            Text(store.journalEntry.isSteelPlateUsed.elementUsedTitle)
+                        }
+                    }
+                    Section(header: Text("Corteza")) {
+                        StarRatingView(staticRating: store.journalEntry.crustRating)
+                    }
+                    Section(header: Text("Miga")) {
+                        StarRatingView(staticRating: store.journalEntry.crumbRating)
+                    }
+                    Section(header: Text("Subida")) {
+                        StarRatingView(staticRating: store.journalEntry.bloomRating)
+                    }
+                    Section(header: Text("Greñado")) {
+                        StarRatingView(staticRating: store.journalEntry.scoreRating)
+                    }
+                    Section(header: Text("Sabor")) {
+                        StarRatingView(staticRating: store.journalEntry.tasteRating)
+                    }
+                    Section(header: Text("Evaluation")) {
+                        StarRatingView(staticRating: store.journalEntry.evaluation)
+                    }
+                }
+                
+                Section {
+                    Button("Delete") {
+                        
+                    }
+                    .foregroundColor(.red)
+                    .frame(maxWidth: .infinity)
+                }
+                
+            }
+            .toolbar {
+                Button("Edit") {
+                    
+                }
+            }
+            .navigationTitle(store.journalEntry.name)
         }
+        
     }
 }
 

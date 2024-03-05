7771e87cf8348e1881c2a60bfc46afb69ea699bb
Michel <michelgoni2@gmail.com>
2024-03-05 19:41:30 +0100

    Added BreadFormTest suite

--- BreadJournalTests/BreadFormTest.swift
+++ BreadJournalTests/BreadFormTest.swift
@@ -13,52 +13,52 @@ import XCTest
 @MainActor
 final class BreadFormTest: XCTestCase {
     
-//    func test_add_ingredients() async {
-//        
-//        let store = TestStore(initialState: BreadFormFeature.State(journalEntry: Entry(id: Entry.ID()))) {
-//            BreadFormFeature()
-//        } withDependencies: {
-//            $0.uuid = .incrementing
-//        }
-//        
-//        await store.send(.addIngredientTapped("")) {
-//            $0.journalEntry.ingredients = [
-//                                Ingredient(
-//                                    id: Ingredient.ID(UUID(0))
-//                                )
-//                            ]
-//        }
-//        
-//    }
-//    
-//    func test_delete_ingredients() async {
-//        let store = TestStore(
-//            initialState: BreadFormFeature.State(
-//                journalEntry: Entry(
-//                    id: Entry.ID(),
-//                    ingredients: [
-//                        Ingredient(
-//                            id: Ingredient.ID(),
-//                            ingredient: "Harina"),
-//                        Ingredient(
-//                            id: Ingredient.ID(),
-//                            ingredient: "Agua")
-//                    ]
-//                )
-//            )
-//        ) {
-//            BreadFormFeature()
-//        } withDependencies: {
-//            $0.uuid = .incrementing
-//        }
-//        
-//        await store.send(.deleteIngredient(atOffset: [0])) {
-//            $0.journalEntry.ingredients =  [$0.journalEntry.ingredients[1]]
-//        }
-//        
-//        await store.send(.deleteIngredient(atOffset: [0])) {
-//            $0.journalEntry.ingredients =  []
-//        }
-//    }
+    func test_add_ingredients() async {
+        
+        let store = TestStore(initialState: BreadFormFeature.State(journalEntry: Entry(id: UUID(.zero)))) {
+            BreadFormFeature()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        
+        await store.send(.addIngredientTapped("")) {
+            $0.journalEntry.ingredients = [
+                                Ingredient(
+                                    id: Ingredient.ID(UUID(0))
+                                )
+                            ]
+        }
+        
+    }
+    
+    func test_delete_ingredients() async {
+        let store = TestStore(
+            initialState: BreadFormFeature.State(
+                journalEntry: Entry(
+                    id: UUID(.zero),
+                    ingredients: [
+                        Ingredient(
+                            id: Ingredient.ID(),
+                            ingredient: "Harina"),
+                        Ingredient(
+                            id: Ingredient.ID(),
+                            ingredient: "Agua")
+                    ]
+                )
+            )
+        ) {
+            BreadFormFeature()
+        } withDependencies: {
+            $0.uuid = .incrementing
+        }
+        
+        await store.send(.deleteIngredient(atOffset: [0])) {
+            $0.journalEntry.ingredients =  [$0.journalEntry.ingredients[1]]
+        }
+        
+        await store.send(.deleteIngredient(atOffset: [0])) {
+            $0.journalEntry.ingredients =  []
+        }
+    }
     
 }

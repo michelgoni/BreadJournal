6bbda55f930fa9c9f4f78f1dc299b3ac006e900b
Michel <michelgoni2@gmail.com>
2024-03-05 19:38:55 +0100

    Added test_delete_alert_confirm_dismisses, test_delete_alert_deletes and test_edit_navigation_state

--- BreadJournalTests/JournaliDetailTests.swift
+++ BreadJournalTests/JournaliDetailTests.swift
@@ -27,64 +27,68 @@ final class JournaliDetailTests: XCTestCase {
             XCTAssertTrue($0.destination?.alert?.buttons.first?.role == .destructive)
         }
     }
-//    
-//    func test_delete_alert_confirm_dismisses() async {
-//        let didDismiss = LockIsolated(false)
-//        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock)) {
-//            JournalDetailViewFeature()
-//        } withDependencies: {
-//            $0.dismiss = DismissEffect({
-//                didDismiss.setValue(true)
-//            })
-//        }
-//        store.exhaustivity = .off
-//        await store.send(.deleteButtonTapped)
-//        await store.send(.destination(.presented(.alert(.confirmDelete)))) {
-//            $0.destination = nil
-//        }
-//    }
-//    
-//    func test_delete_alert_deletes() async {
-//        let didDismiss = LockIsolated(false)
-//        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock)) {
-//            JournalDetailViewFeature()
-//        } withDependencies: {
-//            $0.dismiss = DismissEffect({
-//                didDismiss.setValue(true)
-//            })
-//        }
-//        store.exhaustivity = .off
-//        await store.send(.deleteButtonTapped)
-//        await store.send(.destination(.presented(.alert(.confirmDelete))))
-//        await store.receive(\.delegate.deleteJournalEntry)
-//    }
-//    
-//    func test_edit_navigation_state() async {
-//        
-//        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock)) {
-//            JournalDetailViewFeature()
-//        }
-//        
-//        await store.send(.editButtonTapped) {
-//            $0.destination = .edit(BreadFormFeature.State(journalEntry: .mock))
-//        }
-//        
-//    }
-//    
-//    func test_cancel_navigation_state() async {
-//        
-//        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock)) {
-//            JournalDetailViewFeature()
-//        }
-//        
-//        store.exhaustivity = .off
-//        
-//        await store.send(.cancelEditTapped) {
-//            $0.destination = nil
-//        }
-//        
-//    }
-//    
+    
+    func test_delete_alert_confirm_dismisses() async {
+        let didDismiss = LockIsolated(false)
+        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock,
+                                                                           id: UUID(.zero))) {
+            JournalDetailViewFeature()
+        } withDependencies: {
+            $0.dismiss = DismissEffect({
+                didDismiss.setValue(true)
+            })
+        }
+        store.exhaustivity = .off
+        await store.send(.deleteButtonTapped)
+        await store.send(.destination(.presented(.alert(.confirmDelete)))) {
+            $0.destination = nil
+        }
+    }
+    
+    func test_delete_alert_deletes() async {
+        let didDismiss = LockIsolated(false)
+        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock,
+                                                                           id: UUID(.zero))) {
+            JournalDetailViewFeature()
+        } withDependencies: {
+            $0.dismiss = DismissEffect({
+                didDismiss.setValue(true)
+            })
+        }
+        store.exhaustivity = .off
+        await store.send(.deleteButtonTapped)
+        await store.send(.destination(.presented(.alert(.confirmDelete))))
+        await store.receive(\.delegate.deleteJournalEntry)
+    }
+    
+    func test_edit_navigation_state() async {
+        
+        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock, 
+                                                                           id: UUID(.zero))) {
+            JournalDetailViewFeature()
+        }
+        
+        await store.send(.editButtonTapped) {
+            $0.destination = .edit(BreadFormFeature.State(journalEntry: .mock))
+        }
+        
+    }
+    
+    func test_cancel_navigation_state() async {
+        
+        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: .mock,
+                                                                           id: UUID(.zero))) {
+            JournalDetailViewFeature()
+        }
+        
+        store.exhaustivity = .off
+        
+        await store.send(.cancelEditTapped) {
+            $0.destination = nil
+        }
+        
+    }
+    
 //    func test_edit_entry() async {
 //        var entry = Entry.mock
 //        let store = TestStore(initialState: JournalDetailViewFeature.State(journalEntry: entry)) {
